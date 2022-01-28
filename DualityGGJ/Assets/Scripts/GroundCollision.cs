f87cf78ab5efa5955b3e11926ca1a8c43db78606
Prismly <prismly55@gmail.com>
2022-01-28 12:27:30 -0500

    Fixed jumping, capped move speed, and added tunable acceleration to player
    Additionally - changed the ground tilemap to a composite collider so you don't get caught between tiles when sliding on them

--- DualityGGJ/Assets/Scripts/GroundCollision.cs
+++ DualityGGJ/Assets/Scripts/GroundCollision.cs
@@ -8,10 +8,19 @@ public class GroundCollision : MonoBehaviour
     [SerializeField]
     GameObject Player;
 
-    void OnTriggerEnter2d(Collider2D collision) {
-        if(collision.gameObject.tag == "Ground") {
-            Debug.Log("jumpe trigger");
+    void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.gameObject.tag == "Ground") {
+            Debug.Log("landed");
             Player.GetComponent<Player>().isAirborne = false;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            Debug.Log("left the ground");
+            Player.GetComponent<Player>().isAirborne = true;
         }
     }
 }
--- DualityGGJ/Assets/Scripts/Player.cs
+++ DualityGGJ/Assets/Scripts/Player.cs
@@ -7,15 +7,15 @@ public class Player : MonoBehaviour
     private Rigidbody2D rigidbody;
 
     [SerializeField]
-    public float pSpeed;
+    public float jumpVel, lowGrav, highGrav;
     [SerializeField]
-    public float jumpVel;
-    [SerializeField]
-    public float decelarationConst;
-    public bool isAirborne = false;
+    public float speedCap, accelFactor, decelFactor;
+
+    public bool isAirborne = true;
 
     KeyCode left = KeyCode.LeftArrow;
     KeyCode right = KeyCode.RightArrow;
+    KeyCode jump = KeyCode.UpArrow;
 
     private void Awake()
     {
@@ -25,20 +25,44 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        HorMovement();
+        VerMovement();
+    }
+
+    void HorMovement()
+    {
+        float xVel = rigidbody.velocity.x;
 
         if (Input.GetKey(left))
-            rigidbody.velocity -= new Vector2(pSpeed, 0) * Time.deltaTime;
+            xVel -= accelFactor * Time.deltaTime;
         if (Input.GetKey(right))
-            rigidbody.velocity += new Vector2(pSpeed, 0) * Time.deltaTime;
-  
-        if(rigidbody.velocity.x > 0 && !Input.GetKey(right))
-            rigidbody.velocity -= new Vector2(decelarationConst, 0) * Time.deltaTime;
-        if (rigidbody.velocity.x < 0 && !Input.GetKey(left))
-            rigidbody.velocity += new Vector2(decelarationConst, 0) * Time.deltaTime;
+            xVel += accelFactor * Time.deltaTime;
 
-        if(!isAirborne && Input.GetButtonDown("Jump")) {
+        if (!Input.GetKey(left) && !Input.GetKey(right))
+        {
+            if (xVel > 0.01f)
+                xVel -= decelFactor * Time.deltaTime;
+            if (xVel < -0.01f)
+                xVel += decelFactor * Time.deltaTime;
+        }
+
+        xVel = Mathf.Clamp(xVel, -speedCap, speedCap);
+
+        //Debug.Log(xVel);
+        rigidbody.velocity = new Vector2(xVel, rigidbody.velocity.y);
+    }
+
+    void VerMovement()
+    {
+        if (!isAirborne && Input.GetKeyDown(jump))
+        {
             rigidbody.velocity += Vector2.up * jumpVel;
             isAirborne = true;
         }
+
+        if (rigidbody.velocity.y > 0)
+            rigidbody.gravityScale = lowGrav;
+        else
+            rigidbody.gravityScale = highGrav;
     }
 }
